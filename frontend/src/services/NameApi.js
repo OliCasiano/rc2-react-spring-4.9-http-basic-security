40d7f3096fdb513811ea6057fd21592cd1287607
srb4by <oliversanchezcasiano@gmail.com>
2024-02-05 11:58:24 +0100

    añadir user y pass con el csrf c.disable por que fallaba

--- frontend/src/services/NameApi.js
+++ frontend/src/services/NameApi.js
@@ -1,37 +1,74 @@
 const URL = "http://localhost:8080/members";
-export class NameApi {
-    getMembers() {
-        return fetch(URL)
-            .then(response => response.json())
 
+const username = 'user';
+const password = 'password';
+const basicAuth = 'Basic ' + btoa(username + ':' + password);
+export class NameApi {
+
+    getMembers() {
+        return fetch(URL, {
+            headers: {
+                'Authorization': basicAuth
+            }
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('No autorizado');
+            }
+            return response.json();
+        });
     }
 
     addMember(member) {
         const headers = {
             method: 'post',
-            headers: { 'Content-Type': 'application/json' },
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': basicAuth
+             },
             body: JSON.stringify(member)
         };
         return fetch(URL, headers)
-            .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('No autorizado');
+            }
+            return response.json();
+        });
 
     }
 
     deleteMember(id) {
         const headers = {
-            method: 'delete'
+            method: 'delete',
+            headers: {
+                'Authorization': basicAuth
+            }
         }
         return fetch(URL + "/" + id, headers)
-            .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('No autorizado');
+            }
+            return response.json();
+        });
     }
 
     editMember(id, member) {
         const headers = {
             method: 'put',
-            headers: { "Content-Type": "application/json" },
+            headers: { 
+                "Content-Type": "application/json",
+                'Authorization': basicAuth
+            },
             body: JSON.stringify(member)
         }
         return fetch(URL + "/" + id, headers)
-            .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('No autorizado');
+            }
+            return response.json();
+        });
     }
 }
